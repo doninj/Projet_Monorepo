c97a1c23fff4dc8efb2b17b7ecf56f6a300fa778
doninj <jean-baptiste.38@hotmail.com>
2021-06-17 14:53:39 +0200

    modif de vue config et ajout du fallback

--- client/vue.config.js
+++ client/vue.config.js
@@ -6,13 +6,12 @@ module.exports = {
     devServer: {
       proxy: 'http://localhost:8000/'
     },
-  
     // output built static files to Laravel's public dir.
     // note the "build" script in package.json needs to be modified as well.
-    outputDir: path.resolve(__dirname,'../server/public/dist'),
+    outputDir: path.resolve('../server/public/dist'),
   
     publicPath:'/dist',
     // modify the location of the generated HTML file.
     // make sure to do this only in production.
-    indexPath: path.resolve(__dirname,'../server/resources/views/index.html')
+    indexPath: path.resolve('../server/resources/views/index.html')
   }
--- server/routes/web.php
+++ server/routes/web.php
@@ -14,4 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/{any}', [TestController::class,'app'])->where('any','.*');
+Route::get('/home',function(){
+    return view('welcome');
+});
+Route::fallback(function(){
+    return view('index');
+});
+// Route::get('/{any?}', [TestController::class,'app'])->where('any','.*')->fallback();
