45a4f4332eb430db6989476fd0cec8dac1bd197d
doninj <jean-baptiste.38@hotmail.com>
2021-06-17 15:19:35 +0200

    ajout de route api et test sur le client et le server

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,17 +1,21 @@
 
-
 const path = require('path')
 module.exports = {
-    // proxy API requests to Valet during development
-    devServer: {
-      proxy: 'http://localhost:8000/'
-    },
-    // output built static files to Laravel's public dir.
-    // note the "build" script in package.json needs to be modified as well.
-    outputDir: path.resolve('../server/public/dist'),
-  
-    publicPath:'/dist',
-    // modify the location of the generated HTML file.
-    // make sure to do this only in production.
-    indexPath: path.resolve('../server/resources/views/index.html')
-  }
+  // proxy API requests to Valet during development
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      },
+    }
+  },
+  // output built static files to Laravel's public dir.
+  // note the "build" script in package.json needs to be modified as well.
+  outputDir: path.resolve('../server/public/dist'),
+
+  publicPath: '/dist',
+  // modify the location of the generated HTML file.
+  // make sure to do this only in production.
+  indexPath: path.resolve('../server/resources/views/index.html')
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/coucou',function (Request $request) {
-    return $request->user();
+    return response()->json(["test"=>"test"]);
 });
