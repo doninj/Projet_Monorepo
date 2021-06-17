ec9ddfccec7e1bcd70bb8f01687413336809ed74
doninj <jean-baptiste.38@hotmail.com>
2021-06-17 12:51:30 +0200

    ajout de vue.config

---
+++ client/vue.config.js
@@ -0,0 +1,19 @@
+
+const path = require('path')
+module.exports = {
+    // proxy API requests to Valet during development
+    devServer: {
+      proxy: 'http://localhost:8000/'
+    },
+  
+    // output built static files to Laravel's public dir.
+    // note the "build" script in package.json needs to be modified as well.
+    outputDir: path.resolve(__dirname,'../server/public'),
+  
+    publicPath:'/',
+    // modify the location of the generated HTML file.
+    // make sure to do this only in production.
+    indexPath: process.env.NODE_ENV === 'production'
+      ? '../server/resources/views/index.blade.php'
+      : 'index.html'
+  }
